b66f09915b3c57e0123d6e7250ca4da9d6b4780f
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2023-01-25 21:10:00 +0000

    Asmita Fixed issued in Interventions.selectIntElement where a check statement was incomplete and needed ~isempty(obj) to be added Advection - delEleAdvection and setAdvectionProps modified to reassign advections and array dimensions correctly when deleting elements taking account of flowpath. Element - Changed order in which components are deleted in delElement. Estuary - simialr change to delEleDispersion to reassign dispersion input when deleting elements.
    Added SpitDeltaSEM and WaveRayModel Apps folders
    
    
    
    
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/WaveRayModel@132 5305ae0c-0cd9-424f-b575-fb45b188e99a

---
+++ help/html/example_folder.m
@@ -0,0 +1,11 @@
+function example_folder()
+%find the location of the example folder and open it
+appinfo = matlab.apputil.getInstalledAppInfo;
+idx = find(strcmp({appinfo.name},'AppName'));
+fpath = [appinfo(idx(1)).location,[filesep,'example']];
+try
+    winopen(fpath)
+catch
+    msg = sprintf('The examples can be found here:\n%s',fpath);
+    msgbox(msg)
+end
