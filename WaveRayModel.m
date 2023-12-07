0c15a7a28c8485cd4789a51a5c45f89950eb622a
(no author) <(no author)@5305ae0c-0cd9-424f-b575-fb45b188e99a>
2023-12-07 11:08:10 +0000

    added check4muitoolbox to all Apps. Changes made whilst in China: Added display of line legend using godisplay in multiElePlot Corrected order of check  for saltmarsh flat erosion in ASMinterface.asmitaAreaChange Corrected check for variable area in Inteventions.setAnnualChange typos in mui_open_manual and check4toolbox Add buttondownfunction to multiElePlot Add additional setup checks in Estuary.Response modified tab table position for equilibrium coefficients
    git-svn-id: svn://192.168.0.80/muiApps/Trunk/WaveRayModel@203 5305ae0c-0cd9-424f-b575-fb45b188e99a

--- WaveRayModel.m
+++ WaveRayModel.m
@@ -26,6 +26,9 @@ classdef WaveRayModel < muiModelUI
     methods (Static)
         function obj = WaveRayModel             
             %constructor function initialises GUI
+            isok = check4muitoolbox(obj);
+            if ~isok, return; end
+            %
             obj = setMUI(obj);             
         end
     end
@@ -365,6 +368,29 @@ classdef WaveRayModel < muiModelUI
         function Help(~,~,~)
             doc waveraymodel                             
         end
+
+        %% Check that toolboxes are installed------------------------------
+        function isok = check4muitoolbox(~)
+            %check that dstoolbox and muitoolbox have been installed
+            fname = 'dstable.m';
+            dstbx = which(fname);
+        
+            fname = 'muiModelUI.m';
+            muitbx = which(fname);
+        
+            if isempty(dstbx) && ~isempty(muitbx)
+                warndlg('dstoolbox has not been installed')
+                isok = false;
+            elseif ~isempty(dstbx) && isempty(muitbx)
+                warndlg('muitoolbox has not been installed')
+                isok = false;
+            elseif isempty(dstbx) && isempty(muitbx)
+                warndlg('dstoolbox and muitoolbox have not been installed')
+                isok = false;
+            else
+                isok = true;
+            end
+        end        
     end
 end    
     
